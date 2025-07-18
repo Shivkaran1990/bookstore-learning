8039879e4b61d998519e7289a3d5131a6a4edeeb
Shivkaran Ravidas <s.ravidas01@gmail.com>
2025-07-18 14:28:43 +0530

    minor changes

--- catalog-service/src/main/java/com/skr/learning/bookstore/catalog/domain/ProductService.java
+++ catalog-service/src/main/java/com/skr/learning/bookstore/catalog/domain/ProductService.java
@@ -1,8 +1,21 @@
 package com.skr.learning.bookstore.catalog.domain;
 
 import jakarta.transaction.Transactional;
+
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
-public class ProductService {}
+public class ProductService {
+	   private final ProductRepository productRepository;
+    ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+
+    }
+    public List<ProductEntity> getProducts() {
+    	return productRepository.findAll();
+    	
+    }
+    }
